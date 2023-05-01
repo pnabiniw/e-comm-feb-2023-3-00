82bae5f9a674df49ca539caec0681f1f4e4bef48
npaudyal321 <naween321@gmail.com>
2023-05-01 12:32:46 +0545

    e-comm setup

---
+++ store/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import *
+
+
+def store(request):
+    context = {"products": Product.objects.all()}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        order = {"get_cart_items": 0, "get_cart_total": 0}
+        items = []
+    context = {"items": items, "order": order}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        order = {"get_cart_items": 0, "get_cart_total": 0}
+        items = []
+    context = {"items": items, "order": order}
+    return render(request, 'store/checkout.html', context)
